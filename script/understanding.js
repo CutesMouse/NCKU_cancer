6fc60d8f8a46b223650dede45e0a6f839cef2427
CutesMouse <hfbubble@gmail.com>
2023-06-28 11:22:31 +0800

    20230628 - add next-step buttons to comparison_result.html and understanding.html, and modify the path of favicon.ico.

--- script/understanding.js
+++ script/understanding.js
@@ -5,3 +5,11 @@ function select(id) {
     let selected = (id === 0 ? '.option-small' : '.option-large');
     document.querySelector(selected).classList.add('selected');
 }
+
+function to_comparison() {
+    if (document.querySelector('.selected') === null) {
+        alert('請選擇選項！');
+        return;
+    }
+    window.open('comparison_s1.html', "_self");
+}
